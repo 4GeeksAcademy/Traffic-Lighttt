46f6847077b1233d5d9ba99973546ee9f909eb07
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-02-15 00:59:13 +0000

    botones y rompecabezas

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,33 +1,55 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Semaforo = () => {
-    const [color, setColor] = useState("red")
-	
-	const changeColor = (color) => {
-		setColor(color)
-	}
+	const [color, setColor] = useState("red")
+
+	const [on, setOn] = useState(false)
+
+	useEffect(() => {
+		
+			if (on == false) return null
+
+			if (color == "red") {
+				setTimeout(() => {
+					setColor("green");
+				}, 6000)
+			}
+			else if (color == "green") {
+				setTimeout(() => {
+					setColor("yellow");
+				}, 5000)
+			}
+			else if (color == "yellow") {
+				setTimeout(() => {
+					setColor("red");
+				}, 2000)
+			}
+		
+	}, [color, on]) //esta pendiente de los cambios de el "on" y el "color"
+
 
 	return <>
-	<div className='palo'></div>
+		<div className='palo'></div>
 
 		<div className="semaforo-caja">
-		<div className=
-			{`luz red ${color == "red" ? "brillo" : ""}`} 
-			onClick={() => changeColor("red")}>
-			
+			<div className=
+				{`luz red ${color == "red" ? "brillo" : ""}`}
+				onClick={() => on == true ? ()=>{} : setColor("red")}>
+			</div>
+			<div className=
+				{`luz yellow ${color == "yellow" ? "brillo" : ""}`}
+				onClick={() => on == true ? ()=>{} : setColor("yellow")}>
+			</div>
+			<div className=
+				{`luz green ${color == "green" ? "brillo" : ""}`}
+				onClick={() => on == true ? ()=>{} : setColor("green")}>
+			</div>
 		</div>
-		<div className=
-			{`luz yellow ${color == "yellow" ? "brillo" : ""}`} 
-			onClick={() => changeColor("yellow")}>
+		{ on == false && <button onClick={() => setOn(true)} className="btn btn-danger">Automatico!</button>}
 
-		</div>
-		<div className=
-			{`luz green ${color == "green" ? "brillo" : ""}` }
-			onClick={() => changeColor("green")}>
-
-		</div>
-  	</div>
+		{ on == true && <button onClick={() => setOn(false)} className="btn btn-success">Manual!</button>}
 	</>
+
 }
 
 export default Semaforo
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import Semaforo from "./Semaforo.jsx";
+
 
 
 
